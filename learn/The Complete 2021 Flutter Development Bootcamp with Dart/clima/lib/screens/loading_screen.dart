fd898853697fdda087cacc00520b8ffe857e339c
张开原 <mrkaierwen@gmail.com>
2023-12-11 17:48:51 +0800

    clima：1.完成网络请求；2.完成json数据获取

--- learn/The Complete 2021 Flutter Development Bootcamp with Dart/clima/lib/screens/loading_screen.dart
+++ learn/The Complete 2021 Flutter Development Bootcamp with Dart/clima/lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,12 +10,36 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  // void getWeather() async {
+  //   http.Response response = await http
+  //       .get(Uri.https('api.openweathermap.org', '/data/2.5/weather', {
+  //     'lat': '35',
+  //     'lon': '139',
+  //     'appid': 'a57ec25fa9518f1ad890e63597ab1765',
+  //   }));
+  //   var statusCode = response.statusCode;
+  //   if (statusCode == 200) {
+  //     var body = response.body;
+  //     var result = jsonDecode(body);
+  //     var main = result['weather'][0]['main'];
+  //     var temp = result['main']['temp'];
+  //     var name = result['name'];
+  //     var id = result['weather'][0]['id'];
+  //     print('main = $main');
+  //     print('temp = $temp');
+  //     print('name = $name');
+  //     print('id = $id');
+  //   } else {
+  //     print('error statusCode = $statusCode');
+  //   }
+  // }
+
   @override
   void initState() {
     super.initState();
     var location = Location();
     location.getCurrentLocation();
-    location.getWeather();
+    // print('weather -> $weather.body');
   }
 
   @override
@@ -21,6 +48,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
+            // getWeather();
             // print(getLocation());
           },
           child: Text('Get Location'),
--- learn/The Complete 2021 Flutter Development Bootcamp with Dart/clima/lib/services/location.dart
+++ learn/The Complete 2021 Flutter Development Bootcamp with Dart/clima/lib/services/location.dart
@@ -1,5 +1,6 @@
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
+
+import 'networking.dart';
 
 class Location {
   double? latitude = 0.0;
@@ -11,20 +12,29 @@ class Location {
     try {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.low);
-      print(position);
+      print('position = $position');
+      getWeather(position);
     } catch (e) {
       print('exception: $e');
     }
     // return Future.value(position);
   }
 
-  void getWeather() async {
-    http.Response response = await http
-        .get(Uri.https('samples.openweathermap.org', '/data/2.5/weather', {
-      'lat': '35',
-      'lon': '139',
-      'appid': 'b6907d289e10d714a6e88b30761fae22',
-    }));
-    print(response.body);
+  void getWeather(Position position) async {
+    var weatherUri = Uri.https('api.openweathermap.org', '/data/2.5/weather', {
+      'lat': position.latitude.toString(),
+      'lon': position.longitude.toString(),
+      'appid': 'a57ec25fa9518f1ad890e63597ab1765',
+    });
+    var result = await NetworkHelper(weatherUri).getData();
+    print('data = $result');
+    var main = result['weather'][0]['main'];
+    var temp = result['main']['temp'];
+    var name = result['name'];
+    var id = result['weather'][0]['id'];
+    print('main = $main');
+    print('temp = $temp');
+    print('name = $name');
+    print('id = $id');
   }
 }
